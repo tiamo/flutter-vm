190e0af3d9516d30b3e7ef146a43d4a2c645a53d
tiamo <vk.tiamo@gmail.com>
2022-08-21 15:43:04 +0300

    chore: rename to `vm`

--- test/vm_test.dart
+++ test/vm_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:viewmod/viewmod.dart';
+import 'package:vm/vm.dart';
 
 void main() {
   testWidgets('init view model builder', (tester) async {
