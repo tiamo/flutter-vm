9f1cbe4d5846912b071d1c56f58c44075cfcb7d2
awaik <awaiking44@gmail.com>
2022-08-23 20:26:37 +0300

    feat: example

--- test/vm_test.dart
+++ test/vm_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:vm/vm.dart';
 
 void main() {
