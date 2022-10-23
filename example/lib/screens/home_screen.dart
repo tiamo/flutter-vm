568ca48428c54ef48fc682326aecbc80606f7447
tiamo <vk.tiamo@gmail.com>
2022-10-24 01:09:29 +0300

    fix(ci): deprecation error

--- example/lib/screens/home_screen.dart
+++ example/lib/screens/home_screen.dart
@@ -37,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Text(
                       '${model.counterVal}',
-                      style: Theme.of(context).textTheme.headline4,
+                      style: Theme.of(context).textTheme.headlineMedium,
                     ),
                   ],
                 );
